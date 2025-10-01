359b6786f59d69ad59a5d2e1f7422e2033ac13c7
amirreza azemati <amirrezaazemati@Mac.fritz.box>
2025-10-01 17:19:31 +0200

    game page

--- src/sections/Game/Game.tsx
+++ src/sections/Game/Game.tsx
@@ -4,7 +4,7 @@ import AnimatedBiograthy from "./components/AnimatedBiograthy/AnimatedBiograthy"
 function Game() {
   return (
     <section className="w-screen snap-start flex flex-row items-center h-screen bg-gradient-to-b from-gray-800 via-gray-900 to-black text-white">
-      <div>
+      <div className="w-1/2 h-1/2 bg-white/30 flex justify-center items-center border-2 border-gray-500 m-10 rounded-lg p-4">
         <MinimalGame /> 
       </div>
       <div>
--- src/sections/Game/components/MinimalGame/MinimalGame.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MinimalGame() {
-  return (
-    <div>MinimalGame</div>
-  )
-}
-
-export default MinimalGame
