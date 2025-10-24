20e1b02504779dd4411e05d9e2ab1244ba68e8ee
amirreza azemati <amirreza.azemati@gmail.com>
2025-10-24 10:47:03 +0200

    Home page mobile design adjustment

--- src/sections/Home/Home.tsx
+++ src/sections/Home/Home.tsx
@@ -10,12 +10,12 @@ function Home() {
 
   return (
     <div className="w-screen flex flex-row items-center h-screen text-white">
-      <div className="ml-32 overflow-visible hover:cursor-none transition-all duration-200">
+      <div className="ml-32 z-40 max-sm:ml-24 overflow-visible hover:cursor-none transition-all">
         <Introduction />
       </div>
 
       <div
-        className="relative m-10 z-10 h-11/12 w-1/3 rounded-lg p-4"
+        className="relative z-10 max-sm:-ml-20 m-10 h-11/12 max-sm:w-4/5 max-sm w-1/3 rounded-lg p-4"
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
         onMouseMove={(e) => {
@@ -24,7 +24,9 @@ function Home() {
         }}
         onClick={() => { window.location.href = "#game"; }}
       >
-        <Hero />
+        <div className="w-full h-full flex justify-center items-center -z-50">
+          <Hero />
+        </div>
         {hover && (
           <div
             className="absolute text-xs rounded-full px-4 py-1 backdrop-blur-sm border border-gray-500 "
